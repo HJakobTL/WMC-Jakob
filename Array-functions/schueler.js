6e1eb93ea086946dd7bc54fab5ef73f0ef755cc5
EST2374 <est@archlinux.localdomain>
2026-03-11 13:03:37 +0100

    js arrays

---
+++ Array-functions/schueler.js
@@ -0,0 +1,26 @@
+const students = [
+  { name: "Anna", age: 17, grade: 2 },
+  { name: "Ben", age: 16, grade: 4 },
+  { name: "Clara", age: 18, grade: 1 },
+  { name: "David", age: 17, grade: 5 },
+  { name: "Elena", age: 16, grade: 3 },
+  { name: "Felix", age: 19, grade: 2 },
+  { name: "Gina", age: 17, grade: 1 },
+  { name: "Hugo", age: 18, grade: 4 },
+];
+
+let passed = students.filter( (stud) => stud.grade <= 4);
+
+let labels = students.map( (stud) => `${stud.name} (${stud.age})`);
+
+let passedNames = passed.map((stud) => stud.name);
+
+let avgGrades = students.reduce((acc, stud) =>  { return acc += stud.grade/students.length },0);
+
+let bonus = students.filter((stud) => stud.grade <= 4 && stud.age >= 17).map((stud) => `${stud.name}`).join(", ");
+
+console.log(passed);
+console.log(labels);
+console.log(passedNames);
+console.log(avgGrades);
+console.log(bonus);
